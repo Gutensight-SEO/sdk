11c0ae24b6a58ea490941269ca13d58d1c3d477b
ennas-de <dev.abdulhakeem@gmail.com>
2025-04-23 09:37:57 +0100

    Run final build

--- dist/src/utils/routeParser copy.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Parses the router file to extract route information.
- * @param entryFilePath Path to the router file.
- * @returns Array of normalized routes (path and seoExclude flag).
- */
-export declare function parseRoutes(entryFilePath: string): Promise<{
-    path: string;
-    seoExclude: boolean;
-}[]>;
