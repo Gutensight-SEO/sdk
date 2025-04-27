ba449fb57a144879f300dbe9ed8fcb1a7ba0899c
ennas-de <dev.abdulhakeem@gmail.com>
2025-04-27 12:35:21 +0100

    Getting ready for V1

--- dist/src/constants/index.js
+++ dist/src/constants/index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION_NUMBER = exports.API_URL_PAGE = exports.API_URL_BATCH = void 0;
-// const API_URL = 'https://gs-server-hzfd.onrender.com/api/v1/analyze';
-const API_URL = 'http://localhost:10000/api/v1/analyze';
+const API_URL = 'https://gs-server-hzfd.onrender.com/api/v1/analyze';
+// const API_URL = 'http://localhost:10000/api/v1/analyze';
 exports.API_URL_BATCH = `${API_URL}/pages`;
 exports.API_URL_PAGE = `${API_URL}/page`;
-exports.VERSION_NUMBER = '0.1.4';
+exports.VERSION_NUMBER = '0.1.5';
 //# sourceMappingURL=index.js.map
