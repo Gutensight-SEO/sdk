71e1ef110105a9bcf031b169217734b5388c7728
Abdulhakeem Muhammed <dev.abdulhakeem@gmail.com>
2026-06-16 18:52:09 +0100

    Update dependencies

---
+++ dependency.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash/sh
+
+
+npm i axios commander configstore dotenv fs-extra inquirer luxon
+
+npm i -D @babel/traverse @babel/types @babel/parser @babel/core @types/configstore @types/fs-extra @types/inquirer @types/jest @types/node eslint eslint-config-prettier eslint-plugin-prettier husky jest prettier react ts-jest ts-node typescript
